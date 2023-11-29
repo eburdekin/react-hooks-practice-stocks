c70992befa33d36b5312c066c48aa34229d61c8c
Eileen <eileenburdekin@proton.me>
2023-11-29 11:05:26 -0800

    first deliverables

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
+const stockAPI = 'http://localhost:3001/stocks'
+
 function MainContainer() {
+
+  const [stocks, setStocks] = useState([])
+  const [filter, setFilter] = useState('All')
+
+  //pull in stock's type
+  function handleFilterChange(value) {
+    setFilter(value)
+  }
+
+  //use the stock's type, set as "filter", to filter through "stocks" state
+  const filteredStocks = filter === 'All'
+  ? stocks
+  : stocks.filter(stock => {
+    return stock.type === filter
+  })
+
+  useEffect(() => {
+    fetch(stockAPI)
+    .then(r => r.json())
+    .then(setStocks)
+  }, [])
+
+  function handleClick(clickedStock) {
+    setStocks(stocks.map(stock => {
+    return (clickedStock.id !== stock.id) ? stock
+      : ({...stock, bought: !stock.bought})
+    }
+    ))
+  }
+
+  //use the FILTERED array for displaying. not changing "stocks" itself, just filtering through it before passing that data to components to display
   return (
     <div>
-      <SearchBar />
+      <SearchBar handleFilterChange={handleFilterChange}/>
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={filteredStocks} handleClick={handleClick} />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer boughtStocks={filteredStocks.filter(stock => stock.bought)} handleClick={handleClick}  />
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({handleFilterChange}) {
   return (
     <div>
       <strong>Sort by:</strong>
@@ -27,7 +27,7 @@ function SearchBar() {
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(e) => handleFilterChange(e.target.value)}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
